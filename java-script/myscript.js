9d3b7fb9547ef4e6a0fa5a1df9ea6a5ee9e4be50
Hareesh Kumar <210167425+TheDevOpsHari@users.noreply.github.com>
2025-09-08 10:54:56 +0530

    Add files via upload

---
+++ java-script/myscript.js
@@ -0,0 +1,8 @@
+// window.onload = function () {
+//   document.getElementById("p1").innerHTML = "Luiyp Education";
+// };
+// document.write(300/2);
+// window.alert(15+11);
+alert(15+11);
+// console.log(15+11); 
+
